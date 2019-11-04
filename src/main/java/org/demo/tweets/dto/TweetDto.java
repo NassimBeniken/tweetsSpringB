d921fa1b035eb69bc6056e17082a78a4d504c493
Nassim.Beniken <nassim.ben-iken@etu.u-pec.fr>
2019-11-04 14:11:44 +0100

    Commentaires dans la classe qui permettent d'en savoir plus sur les annotations utilisées

--- src/main/java/org/demo/tweets/dto/TweetDto.java
+++ src/main/java/org/demo/tweets/dto/TweetDto.java
@@ -13,8 +13,36 @@ import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
-@Data //Permet de générer tous les getters et setter sans les ecrire (grace a lombok)t
+/*
+    Provient de Lombok
+    Contient les annotations @Getter, @Setter, @ToString, @EqualsAndHashCode et @RequiredArgsConstructor
+    permet de générer toutes ces méthodes sans rien faire, tout est déjà écrit
+ */
+@Data
+/*
+    Provient de Lombok
+    Permet de créer des instances de notre classe sans avoir besoin de constructeur
+    en utiisant seulement le code suivant :
+    Tweet newTweet = Tweet.builder()
+        .id(1)
+        .created("date")
+        .modified("date")
+        .text("text du tweet")
+        .user(User)
+        .source(Source)
+        .etag("etag")
+        .build();
+
+    On peut creer des copies ou quasi copies de nos instances en utilisant @Builder(toBuilder = true)
+    Ca permet de pouvoir faire ça :
+    Tweet.TweetBuilder tweetBuilder = newTweet.toBuilder(); //Ici du coup on a exactement la même chose que newTweet
+    Tweet copieTweet = tweetBuilder.id(2).build(); //Ici du coup on a le meme que newTweet mais avec id=2
+ */
 @Builder
+/*
+    Provient de Lombok
+    Permet de générer un constructeur avec tous les arguments
+ */
 @AllArgsConstructor
 public class TweetDto {
 
@@ -28,6 +56,9 @@ public class TweetDto {
     String id;
     @NotNull
     private Source source;
+    /*
+        Décide comment l'attribut sera sérialisé
+     */
     @JsonFormat(pattern = ZONE_DATE_TIME_FORMAT)
     ZonedDateTime created;
     @JsonFormat(pattern = ZONE_DATE_TIME_FORMAT)
--- src/main/java/org/demo/tweets/dto/UserDto.java
+++ src/main/java/org/demo/tweets/dto/UserDto.java
@@ -10,14 +10,52 @@ import org.demo.tweets.model.User;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+/*
+    Provient de Lombok
+    Contient les annotations @Getter, @Setter, @ToString, @EqualsAndHashCode et @RequiredArgsConstructor
+    permet de générer toutes ces méthodes sans rien faire, tout est déjà écrit
+ */
 @Data
+/*
+    Provient de Lombok
+    Permet de créer des instances de notre classe sans avoir besoin de constructeur
+    en utiisant seulement le code suivant :
+    Tweet newTweet = Tweet.builder()
+        .id(1)
+        .created("date")
+        .modified("date")
+        .text("text du tweet")
+        .user(User)
+        .source(Source)
+        .etag("etag")
+        .build();
+
+    On peut creer des copies ou quasi copies de nos instances en utilisant @Builder(toBuilder = true)
+    Ca permet de pouvoir faire ça :
+    Tweet.TweetBuilder tweetBuilder = newTweet.toBuilder(); //Ici du coup on a exactement la même chose que newTweet
+    Tweet copieTweet = tweetBuilder.id(2).build(); //Ici du coup on a le meme que newTweet mais avec id=2
+ */
 @Builder
+/*
+    Provient de Lombok aussi je crois
+    Permet de générer un constructeur sans arguments
+ */
 @NoArgsConstructor
+/*
+    Provient de Lombok
+    Permet de générer un constructeur avec tous les arguments
+ */
 @AllArgsConstructor
+/*
+    Indique que toutes les propriétés de la classe doivent être sérialisées et non null du coup je crois
+ */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDto {
     @NotBlank
     private String nickname;
+    /*
+        Permet de valider que cet attribut est une adresse mail valide
+     */
     @Email
     private String mail;
 
